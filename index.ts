12b3adf38df3a8bab4bf3c2c67ee1ec4be932144
Frederik Schöll <fschoell@posteo.de>
2020-03-30 12:28:02 +0200

    stop script on errors, add tsconfig

--- index.ts
+++ index.ts
@@ -1,38 +1,42 @@
-import { CronJob } from "cron";
-import { getBpjson, getProducerJson, createHash, setProducerJson, transact } from "./src/utils";
-import { rpc, BPJSON_ENDPOINT, PRODUCER_ACCOUNT_NAME, TIMEZONE, CRON_TIME, PRODUCERJSON } from "./src/config";
+import {CronJob} from "cron";
+import {createHash, getBpjson, getProducerJson, setProducerJson, transact} from "./src/utils";
+import {BPJSON_ENDPOINT, CRON_TIME, PRODUCER_ACCOUNT_NAME, PRODUCERJSON, rpc, TIMEZONE} from "./src/config";
 
 async function main() {
-  // Fetch bp.json from website
-  const bpjson = await getBpjson(BPJSON_ENDPOINT);
-  if (bpjson.producer_account_name !== PRODUCER_ACCOUNT_NAME) {
-    throw new Error("[PRODUCER_ACCOUNT_NAME] does not match bp.json");
-  }
-
-  // Check if producer account exists
-  try {
-    await rpc.get_account(PRODUCER_ACCOUNT_NAME)
-  } catch (e) {
-    throw new Error(`[PRODUCER_ACCOUNT_NAME => ${PRODUCER_ACCOUNT_NAME}] does not exist`);
-  }
-
-  // Check if on-chain is different from website
-  const producerjson = await getProducerJson(PRODUCER_ACCOUNT_NAME);
-
-  if (producerjson) {
-    console.log("comparing hashes", createHash(bpjson), createHash(producerjson));
-    if (createHash(bpjson) === createHash(producerjson)) {
-      throw new Error(`[${PRODUCERJSON}] & [bp.json] hashes are the same, nothing to update`);
+    // Fetch bp.json from website
+    const bpjson = await getBpjson(BPJSON_ENDPOINT);
+    if (bpjson.producer_account_name !== PRODUCER_ACCOUNT_NAME) {
+        throw new Error("[PRODUCER_ACCOUNT_NAME] does not match bp.json");
     }
-  } else {
-    console.log('first time publishing');
-  }
 
-  // Push actions
-  const action = setProducerJson(PRODUCER_ACCOUNT_NAME, bpjson);
-  await transact([action]);
+    // Check if producer account exists
+    try {
+        await rpc.get_account(PRODUCER_ACCOUNT_NAME)
+    } catch (e) {
+        throw new Error(`[PRODUCER_ACCOUNT_NAME => ${PRODUCER_ACCOUNT_NAME}] does not exist`);
+    }
+
+    // Check if on-chain is different from website
+    const producerjson = await getProducerJson(PRODUCER_ACCOUNT_NAME);
+
+    if (producerjson) {
+        console.log("comparing hashes", createHash(bpjson), createHash(producerjson));
+        if (createHash(bpjson) === createHash(producerjson)) {
+            console.log(`[${PRODUCERJSON}] & [bp.json] hashes are the same, nothing to update`);
+        }
+    } else {
+        console.log('first time publishing');
+    }
+
+    // Push actions
+    const action = setProducerJson(PRODUCER_ACCOUNT_NAME, bpjson);
+    await transact([action]);
 }
 
 new CronJob(CRON_TIME, async () => {
-  main().catch(e => console.error(e.message));
-}, () => {}, true, TIMEZONE);
+    main().catch(e => {
+        console.error(e.message);
+        process.exit(1);
+    });
+}, () => {
+}, true, TIMEZONE);
